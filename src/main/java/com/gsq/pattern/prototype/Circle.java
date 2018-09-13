dd1f6259076406a52385fd9fd25a9071845aa3b7
guishangquan <guishangquan@szzbmy.com>
2018-09-13 11:50:09 +0800

    原型模式

---
+++ src/main/java/com/gsq/pattern/prototype/Circle.java
@@ -0,0 +1,16 @@
+package com.gsq.pattern.prototype;
+
+/**
+ * 原型模式
+ */
+public class Circle extends Shape {
+
+    public Circle(){
+        type = "Circle";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
---
+++ src/main/java/com/gsq/pattern/prototype/Rectangle.java
@@ -0,0 +1,16 @@
+package com.gsq.pattern.prototype;
+
+/**
+ * 原型模式
+ */
+public class Rectangle extends Shape {
+
+    public Rectangle(){
+        type = "Rectangle";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
